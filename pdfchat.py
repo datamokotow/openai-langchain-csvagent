785c67ff9e7d3cdb9f68565cc98fe32631c48e29
datamokotow <a_rutvik@yahoo.com>
2023-05-26 01:12:10 +0530

    Update pdfchat.py

--- pdfchat.py
+++ pdfchat.py
@@ -1,121 +1,206 @@
-from PyPDF2 import PdfReader
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationalRetrievalChain
-import pickle
-from pathlib import Path
-from dotenv import load_dotenv
-import os
+# Import necessary modules
+import re
+import time
+from io import BytesIO
+from typing import Any, Dict, List
+
+import openai
 import streamlit as st
-from streamlit_chat import message
-import io
-import asyncio
+from langchain import LLMChain, OpenAI
+from langchain.agents import AgentExecutor, Tool, ZeroShotAgent
+from langchain.chains import RetrievalQA
+from langchain.chains.question_answering import load_qa_chain
+from langchain.docstore.document import Document
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import VectorStore
+from langchain.vectorstores.faiss import FAISS
+from pypdf import PdfReader
 
-load_dotenv()
-api_key = os.getenv('OPENAI_API_KEY')  
 
-# vectors = getDocEmbeds("gpt4.pdf")
-# qa = ChatVectorDBChain.from_llm(ChatOpenAI(model_name="gpt-3.5-turbo"), vectors, return_source_documents=True)
+# Define a function to parse a PDF file and extract its text content
+@st.cache_data
+def parse_pdf(file: BytesIO) -> List[str]:
+    pdf = PdfReader(file)
+    output = []
+    for page in pdf.pages:
+        text = page.extract_text()
+        # Merge hyphenated words
+        text = re.sub(r"(\w+)-\n(\w+)", r"\1\2", text)
+        # Fix newlines in the middle of sentences
+        text = re.sub(r"(?<!\n\s)\n(?!\s\n)", " ", text.strip())
+        # Remove multiple newlines
+        text = re.sub(r"\n\s*\n", "\n\n", text)
+        output.append(text)
+    return output
 
-async def main():
 
-    async def storeDocEmbeds(file, filename):
+# Define a function to convert text content to a list of documents
+@st.cache_data
+def text_to_docs(text: str) -> List[Document]:
+    """Converts a string or list of strings to a list of Documents
+    with metadata."""
+    if isinstance(text, str):
+        # Take a single string as one page
+        text = [text]
+    page_docs = [Document(page_content=page) for page in text]
+
+    # Add page numbers as metadata
+    for i, doc in enumerate(page_docs):
+        doc.metadata["page"] = i + 1
+
+    # Split pages into chunks
+    doc_chunks = []
+
+    for doc in page_docs:
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=2000,
+            separators=["\n\n", "\n", ".", "!", "?", ",", " ", ""],
+            chunk_overlap=0,
+        )
+        chunks = text_splitter.split_text(doc.page_content)
+        for i, chunk in enumerate(chunks):
+            doc = Document(
+                page_content=chunk, metadata={"page": doc.metadata["page"], "chunk": i}
+            )
+            # Add sources a metadata
+            doc.metadata["source"] = f"{doc.metadata['page']}-{doc.metadata['chunk']}"
+            doc_chunks.append(doc)
+    return doc_chunks
+
+
+# Define a function for the embeddings
+@st.cache_data
+def test_embed():
+    embeddings = OpenAIEmbeddings(openai_api_key=api)
+    # Indexing
+    # Save in a Vector DB
+    with st.spinner("It's indexing..."):
+        index = FAISS.from_documents(pages, embeddings)
+    st.success("Embeddings done.", icon="✅")
+    return index
+
+
+# Set up the Streamlit app
+st.title("🤖 Personalized Bot with Memory 🧠 ")
+st.markdown(
+    """ 
+        ####  🗨️ Chat with your PDF files 📜 with `Conversational Buffer Memory`  
+        > *powered by [LangChain]('https://langchain.readthedocs.io/en/latest/modules/memory.html#memory') + 
+        [OpenAI]('https://platform.openai.com/docs/models/gpt-3-5') + [DataButton](https://www.databutton.io/)*
+        ----
+        """
+)
+
+st.markdown(
+    """
+    `openai`
+    `langchain`
+    `tiktoken`
+    `pypdf`
+    `faiss-cpu`
     
-        reader = PdfReader(file)
-        corpus = ''.join([p.extract_text() for p in reader.pages if p.extract_text()])
-        
-        splitter =  RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200,)
-        chunks = splitter.split_text(corpus)
-        
-        embeddings = OpenAIEmbeddings(openai_api_key = api_key)
-        vectors = FAISS.from_texts(chunks, embeddings)
-        
-        with open(filename + ".pkl", "wb") as f:
-            pickle.dump(vectors, f)
+    ---------
+    """
+)
 
-        
-    async def getDocEmbeds(file, filename):
-        
-        if not os.path.isfile(filename + ".pkl"):
-            await storeDocEmbeds(file, filename)
-        
-        with open(filename + ".pkl", "rb") as f:
-            global vectores
-            vectors = pickle.load(f)
-            
-        return vectors
-    
+# Set up the sidebar
+st.sidebar.markdown(
+    """
+    ### Steps:
+    1. Upload PDF File
+    2. Enter Your Secret Key for Embeddings
+    3. Perform Q&A
 
-    async def conversational_chat(query):
-        result = qa({"question": query, "chat_history": st.session_state['history']})
-        st.session_state['history'].append((query, result["answer"]))
-        # print("Log: ")
-        # print(st.session_state['history'])
-        return result["answer"]
+    **Note : File content and API key not stored in any form.**
+    """
+)
 
+# Allow the user to upload a PDF file
+uploaded_file = st.file_uploader("**Upload Your PDF File**", type=["pdf"])
 
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo")
-    chain = load_qa_chain(llm, chain_type="stuff")
+if uploaded_file:
+    name_of_file = uploaded_file.name
+    doc = parse_pdf(uploaded_file)
+    pages = text_to_docs(doc)
+    if pages:
+        # Allow the user to select a page and view its content
+        with st.expander("Show Page Content", expanded=False):
+            page_sel = st.number_input(
+                label="Select Page", min_value=1, max_value=len(pages), step=1
+            )
+            pages[page_sel - 1]
+        # Allow the user to enter an OpenAI API key
+        api = st.text_input(
+            "**Enter OpenAI API Key**",
+            type="password",
+            placeholder="sk-",
+            help="https://platform.openai.com/account/api-keys",
+        )
+        if api:
+            # Test the embeddings and save the index in a vector database
+            index = test_embed()
+            # Set up the question-answering system
+            qa = RetrievalQA.from_chain_type(
+                llm=OpenAI(openai_api_key=api),
+                chain_type = "map_reduce",
+                retriever=index.as_retriever(),
+            )
+            # Set up the conversational agent
+            tools = [
+                Tool(
+                    name="State of Union QA System",
+                    func=qa.run,
+                    description="Useful for when you need to answer questions about the aspects asked. Input may be a partial or fully formed question.",
+                )
+            ]
+            prefix = """Have a conversation with a human, answering the following questions as best you can based on the context and memory available. 
+                        You have access to a single tool:"""
+            suffix = """Begin!"
 
-    if 'history' not in st.session_state:
-        st.session_state['history'] = []
+            {chat_history}
+            Question: {input}
+            {agent_scratchpad}"""
 
+            prompt = ZeroShotAgent.create_prompt(
+                tools,
+                prefix=prefix,
+                suffix=suffix,
+                input_variables=["input", "chat_history", "agent_scratchpad"],
+            )
 
-    #Creating the chatbot interface
-    st.title("PDFChat :")
+            if "memory" not in st.session_state:
+                st.session_state.memory = ConversationBufferMemory(
+                    memory_key="chat_history"
+                )
 
-    if 'ready' not in st.session_state:
-        st.session_state['ready'] = False
+            llm_chain = LLMChain(
+                llm=OpenAI(
+                    temperature=0, openai_api_key=api, model_name="gpt-3.5-turbo"
+                ),
+                prompt=prompt,
+            )
+            agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
+            agent_chain = AgentExecutor.from_agent_and_tools(
+                agent=agent, tools=tools, verbose=True, memory=st.session_state.memory
+            )
 
-    uploaded_file = st.file_uploader("Choose a file", type="pdf")
+            # Allow the user to enter a query and generate a response
+            query = st.text_input(
+                "**What's on your mind?**",
+                placeholder="Ask me anything from {}".format(name_of_file),
+            )
 
-    if uploaded_file is not None:
+            if query:
+                with st.spinner(
+                    "Generating Answer to your Query : `{}` ".format(query)
+                ):
+                    res = agent_chain.run(query)
+                    st.info(res, icon="🤖")
 
-        with st.spinner("Processing..."):
-        # Add your code here that needs to be executed
-            uploaded_file.seek(0)
-            file = uploaded_file.read()
-            # pdf = PyPDF2.PdfFileReader()
-            vectors = await getDocEmbeds(io.BytesIO(file), uploaded_file.name)
-            qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model_name="gpt-3.5-turbo"), retriever=vectors.as_retriever(), return_source_documents=True)
-
-        st.session_state['ready'] = True
-
-    st.divider()
-
-    if st.session_state['ready']:
-
-        if 'generated' not in st.session_state:
-            st.session_state['generated'] = ["Welcome! You can now ask any questions regarding " + uploaded_file.name]
-
-        if 'past' not in st.session_state:
-            st.session_state['past'] = ["Hey!"]
-
-        # container for chat history
-        response_container = st.container()
-
-        # container for text box
-        container = st.container()
-
-        with container:
-            with st.form(key='my_form', clear_on_submit=True):
-                user_input = st.text_input("Query:", placeholder="e.g: Summarize the paper in a few sentences", key='input')
-                submit_button = st.form_submit_button(label='Send')
-
-            if submit_button and user_input:
-                output = await conversational_chat(user_input)
-                st.session_state['past'].append(user_input)
-                st.session_state['generated'].append(output)
-
-        if st.session_state['generated']:
-            with response_container:
-                for i in range(len(st.session_state['generated'])):
-                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
-                    message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+            # Allow the user to view the conversation history and other information stored in the agent's memory
+            with st.expander("History/Memory"):
+                st.session_state.memory
