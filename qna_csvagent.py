ccaa09bfbfb93f992a2caff362e0bb40ba4dc310
datamokotow <a_rutvik@yahoo.com>
2023-05-25 22:04:20 +0530

    Update qna_csvagent.py

--- qna_csvagent.py
+++ qna_csvagent.py
@@ -1,53 +1,161 @@
-import os
 import streamlit as st
-from langchain.agents import create_csv_agent
-from langchain.llms import OpenAI
-from streamlit_chat import message
+import pandas as pd
+import json
 
-st.sidebar.title("OpenAI API Key")
-api_key = st.sidebar.text_input("Enter your API key:", type="password")
+from langchain import OpenAI
+from langchain.agents import create_pandas_dataframe_agent
+import pandas as pd
 
-uploaded_file = st.sidebar.file_uploader("Upload a CSV file", type=["csv"])
+# Setting up the api key
+import environ
 
-st.sidebar.title("Follow Me")
-st.sidebar.markdown(
+env = environ.Env()
+environ.Env.read_env()
+
+OPENAI_API_KEY = env("OPENAI_API_KEY")
+
+
+def create_agent(filename: str):
     """
-    * [Twitter](https://twitter.com/datamokotow)
-    * [GitHub](https://github.com/datamokotow)
+    Create an agent that can access and use a large language model (LLM).
+
+    Args:
+        filename: The path to the CSV file that contains the data.
+
+    Returns:
+        An agent that can access and use the LLM.
     """
-)
 
-if api_key:
-    os.environ["OPENAI_API_KEY"] = api_key
-    st.title("OpenAI-Langchain CSV Agent Interaction")
+    # Create an OpenAI object.
+    llm = OpenAI(openai_api_key=OPENAI_API_KEY)
 
-    if uploaded_file:
-        st.write("CSV file uploaded successfully!")
+    # Read the CSV file into a Pandas DataFrame.
+    df = pd.read_csv(filename)
 
-        agent = create_csv_agent(
-            OpenAI(temperature=0, client=any), uploaded_file, verbose=True
-        )
+    # Create a Pandas DataFrame agent.
+    return create_pandas_dataframe_agent(llm, df, verbose=False)
 
-        # Initialize the chat history in the session_state if it doesn't exist
-        if "chat_history" not in st.session_state:
-            st.session_state.chat_history = []
 
-        user_input = st.text_input("Enter your question:", key="input_field")
+def query_agent(agent, query):
+    """
+    Query an agent and return the response as a string.
 
-        if user_input:
-            answer = agent.run(user_input)
-            # Add the question and answer to the chat_history
-            st.session_state.chat_history.append(("user", user_input))
-            st.session_state.chat_history.append(("agent", answer))
+    Args:
+        agent: The agent to query.
+        query: The query to ask the agent.
 
-        # Display the chat_history in a chat-like format using streamlit-chat
-        for i, (sender, message_text) in enumerate(st.session_state.chat_history):
-            if sender == "user":
-                message(message_text, is_user=True, key=f"{i}_user")
-            else:
-                message(message_text, key=f"{i}")
+    Returns:
+        The response from the agent as a string.
+    """
 
-    else:
-        st.write("Please upload a CSV file.")
-else:
-    st.sidebar.error("Please enter your OpenAI API key.")
+    prompt = (
+        """
+            For the following query, if it requires drawing a table, reply as follows:
+            {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}}
+
+            If the query requires creating a bar chart, reply as follows:
+            {"bar": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
+            
+            If the query requires creating a line chart, reply as follows:
+            {"line": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
+            
+            There can only be two types of chart, "bar" and "line".
+            
+            If it is just asking a question that requires neither, reply as follows:
+            {"answer": "answer"}
+            Example:
+            {"answer": "The title with the highest rating is 'Gilead'"}
+            
+            If you do not know the answer, reply as follows:
+            {"answer": "I do not know."}
+            
+            Return all output as a string.
+            
+            All strings in "columns" list and data list, should be in double quotes,
+            
+            For example: {"columns": ["title", "ratings_count"], "data": [["Gilead", 361], ["Spider's Web", 5164]]}
+            
+            Lets think step by step.
+            
+            Below is the query.
+            Query: 
+            """
+        + query
+    )
+
+    # Run the prompt through the agent.
+    response = agent.run(prompt)
+
+    # Convert the response to a string.
+    return response.__str__()
+
+
+
+def decode_response(response: str) -> dict:
+    """This function converts the string response from the model to a dictionary object.
+
+    Args:
+        response (str): response from the model
+
+    Returns:
+        dict: dictionary with response data
+    """
+    return json.loads(response)
+
+
+def write_response(response_dict: dict):
+    """
+    Write a response from an agent to a Streamlit app.
+
+    Args:
+        response_dict: The response from the agent.
+
+    Returns:
+        None.
+    """
+
+    # Check if the response is an answer.
+    if "answer" in response_dict:
+        st.write(response_dict["answer"])
+
+    # Check if the response is a bar chart.
+    if "bar" in response_dict:
+        data = response_dict["bar"]
+        df = pd.DataFrame(data)
+        df.set_index("columns", inplace=True)
+        st.bar_chart(df)
+
+    # Check if the response is a line chart.
+    if "line" in response_dict:
+        data = response_dict["line"]
+        df = pd.DataFrame(data)
+        df.set_index("columns", inplace=True)
+        st.line_chart(df)
+
+    # Check if the response is a table.
+    if "table" in response_dict:
+        data = response_dict["table"]
+        df = pd.DataFrame(data["data"], columns=data["columns"])
+        st.table(df)
+
+
+st.title("👨‍💻 Chat with your CSV")
+
+st.write("Please upload your CSV file below.")
+
+data = st.file_uploader("Upload a CSV")
+
+query = st.text_area("Insert your query")
+
+if st.button("Submit Query", type="primary"):
+    # Create an agent from the CSV file.
+    agent = create_agent(data)
+
+    # Query the agent.
+    response = query_agent(agent=agent, query=query)
+
+    # Decode the response.
+    decoded_response = decode_response(response)
+
+    # Write the response to the Streamlit app.
+    write_response(decoded_response)
